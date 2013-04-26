cc484bc13ca2c8b0933f2ab29e0439a143a225c8
Nick Miceli <ngmiceli@google.com>
2013-04-26 10:45:36 -0400

    Get a working Hello World app

---
+++ src/nmiceli/martine/HelloWorldServlet.java
@@ -0,0 +1,13 @@
+package nmiceli.martine;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class HelloWorldServlet extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    resp.setContentType("text/plain");
+    resp.getWriter().println("Hello, world");
+  }
+}
