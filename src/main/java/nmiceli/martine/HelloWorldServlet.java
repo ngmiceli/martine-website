c50bbce33004ff1c80f01fb81e4e672498009a4c
Nick Miceli <ngmiceli@google.com>
2013-05-01 16:41:24 -0400

    Changed webapp to war, removed generated code from src

--- src/main/java/nmiceli/martine/HelloWorldServlet.java
+++ src/main/java/nmiceli/martine/HelloWorldServlet.java
@@ -8,6 +8,6 @@ public class HelloWorldServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     resp.setContentType("text/plain");
-    resp.getWriter().println("Hello, world");
+    resp.getWriter().println("Hello, world. This is a test");
   }
 }
